f76d34b2b7a711736fab6cb9d537561e4c77b99c
sun-vm <jmw686@nyu.edu>
2018-08-04 12:50:13 -0500

    add some static pages--no content

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    # render 'static_pages/home'
+  end
+
+  def terms
+  end
+  def support
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
+  get '/terms', to: 'static_pages#terms'
+  get '/support', to: 'static_pages#support'
 end
